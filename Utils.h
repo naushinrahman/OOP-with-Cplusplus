8cd022d2cab37ecf856324fb424cd8481db962a3
naushinrahman <130614154+naushinrahman@users.noreply.github.com>
2023-07-24 15:22:29 -0400

    Add files via upload
    Learning the basics of modular, object oriented programming

---
+++ Utils.h
@@ -0,0 +1,12 @@
+#ifndef _SDDS_UTILS_H_
+#define _SDDS_UTILS_H_
+
+namespace sdds {
+   void flushkeys();
+   bool ValidYesResponse(char ch);
+   bool yes();
+   void readCstr(char cstr[], int len);
+   int readInt(int min, int max);
+}
+#endif
+
