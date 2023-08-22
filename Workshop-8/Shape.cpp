b87a6f0be0f59d92ddef63152faf48323155cb1a
naushinrahman <130614154+naushinrahman@users.noreply.github.com>
2023-08-22 15:33:29 -0400

    Create Shape.cpp

---
+++ Workshop-8/Shape.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Shape.h"
+using namespace std;
+
+namespace sdds {
+
+std::ostream& operator<<(std::ostream& os, const Shape& shape) {
+    shape.draw(os);
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Shape& shape) {
+   shape.getSpecs(is);
+   return is;
+}
+
+
+}
